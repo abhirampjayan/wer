24d979abc3a533314bd67c7a66a808bae06dc2b6
abhirampjayan <abhiramp08@gmail.com>
2019-09-19 15:38:25 +0530

    kk

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 def index():
     return render_template('home.html')
 @app.route("/about")
-def index():
+def about():
     return render_template('index.html')
 if(__name__=="__main__"):
     app.run(debug='true')
