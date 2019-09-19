8dafae0463888a3e0d5344adc822cef1bebd629b
abhirampjayan <abhiramp08@gmail.com>
2019-09-19 14:26:06 +0530

    kk

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
+@app.route("/")
+def index():
+    return render_template('home.html')
+@app.route("/about")
+def index():
+    return render_template('index.html')
 if(__name__=="__main__"):
-    app.run()
+    app.run(debug='true')
